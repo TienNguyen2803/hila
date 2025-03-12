84d20e04a829e4ff69ae126681f0bde8fe39c9b2
TienNM <Tiennm@splus-software.com.vn>
2025-03-12 17:18:38 +0700

    - Update import pages/NotFound.tsx

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -6,9 +6,9 @@ import AppLayout from '../layout/AppLayout';
 
 const Home = lazy(() => import('../pages/Home'));
 const About = lazy(() => import('../pages/About'));
-const Services = lazy(() => import('../pages/Services'));
+// const Services = lazy(() => import('../pages/Services'));
 const Contact = lazy(() => import('../pages/Contact'));
-const NotFound = lazy(() => import('../pages/NotFound'));
+const NotFound = lazy(() => import('../pages/notFound/index'));
 
 const AppRouter: React.FC = () => {
   return (
@@ -17,7 +17,7 @@ const AppRouter: React.FC = () => {
         <Route path="/" element={<AppLayout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="services" element={<Services />} />
+          {/* <Route path="services" element={<Services />} /> */}
           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
         </Route>
